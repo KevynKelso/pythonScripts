eaba55aff0314f27389951a5580a78ba186db949
Kevyn Kelso <kkelso@bscs.org>
2020-02-18 11:25:03 -0700

    start script for remove title checker (span line)

---
+++ functions.py
@@ -0,0 +1,13 @@
+# import random
+# import string
+
+
+# def randomString(stringLength):
+#     letters = string.ascii_lowercase
+#     return ''.join(random.choice(letters) for i in range(stringLength))
+
+    
+# def fillWithText():
+#     f = open("test.txt", "a")
+#     f.write(randomString(100)
+   
---
+++ testingFunctions.py
@@ -0,0 +1,4 @@
+def my_function():
+    print("yooooo")
+
+my_function()
