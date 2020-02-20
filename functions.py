603c841cce1e8349e46e93cca07bc8152c231add
Kevyn Kelso <kkelso@bscs.org>
2020-02-20 12:56:10 -0700

    finish code, functions work

--- functions.py
+++ functions.py
@@ -1,13 +1,58 @@
-# import random
-# import string
+import os
+import random
+import string
 
 
-# def randomString(stringLength):
-#     letters = string.ascii_lowercase
-#     return ''.join(random.choice(letters) for i in range(stringLength))
+def randomString(stringLength=10):
+    letters = string.ascii_lowercase
+    return ''.join(random.choice(letters) for i in range(stringLength))
 
-    
-# def fillWithText():
-#     f = open("test.txt", "a")
-#     f.write(randomString(100)
-   
+
+def fillWithText(fileName):
+    f = open(fileName, "a")
+    for i in range(100):
+        f.write("\n")
+        f.write(randomString(100))
+    f.close()
+
+
+def insertOnFirstLine(Line1, fileName):
+    file1 = open(fileName, "r")
+    oline = file1.readlines()
+    oline.insert(0, Line1)
+    file1.close()
+    file1 = open(fileName, "w")
+    file1.writelines(oline)
+    file1.writelines("\n")
+    file1.close()
+
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
+
+
+def insertLineInAllFiles(directoryPath, line):
+    allFiles = getListOfFiles(directoryPath)
+    for file in allFiles:
+            if file.endswith(".html"):
+                # Check and insert statement
+                with open(file) as content_file:
+                    content = content_file.read()
+                    if line in content:
+                        print(file + " already contains " + line)
+                    else:    
+                        insertOnFirstLine(line, file) 
--- span_adder.py
+++ span_adder.py
@@ -1,41 +1,6 @@
-import random
-import string
+from functions import insertLineInAllFiles
 
 
-
-def randomString(stringLength=10):
-    letters = string.ascii_lowercase
-    return ''.join(random.choice(letters) for i in range(stringLength))
-
-    
-def fillWithText():
-    f = open("test.txt", "a")
-    for i in range(100):
-        f.write(randomString(100))
-        f.write("\n")
-    f.close()
-
-
-def insertOnFirstLine(spanLine1, file1Name):
-    file1 = open(file1Name, "r")
-    oline = file1.readlines()
-    oline.insert(0, spanLine1)
-    file1.close()
-    file1 = open(file1Name, "w")
-    file1.writelines(oline)
-    file1.close()
-
-        
-fillWithText()
 spanLine1 = "<span class=\"remove-page-title\"></span>"
-file1Name = "test.txt"
-insertOnFirstLine(spanLine1, file1Name)
-# f = open("test.txt", "r")
-# spanLine1 = "<span class=\"remove-page-title\"></span>"
-# oline = f.readlines()
-# oline.insert(0, spanLine1)
-# f.close()
-# f = open("test.txt", "w")
-# f.writelines(oline)
-# f.close()
-# print(f.read())
+directoryPath = "/Users/kkelso/Desktop/projects/python/scripts"        
+insertLineInAllFiles(directoryPath, spanLine1)
